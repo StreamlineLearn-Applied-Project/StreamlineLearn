0b0b722588c11297e58c15f159e7afa021d2e26e
Abishek Alwis <abishekalwis.career@gmail.com>
2024-05-04 15:11:29 +0530

    reconstruct the Assessment Submission Service

--- AssessmentSubmissionService/src/main/java/com/StreamlineLearn/AssessmentSubmissionService/model/Submission.java
+++ AssessmentSubmissionService/src/main/java/com/StreamlineLearn/AssessmentSubmissionService/model/Submission.java
@@ -1,6 +1,8 @@
 package com.StreamlineLearn.AssessmentSubmissionService.model;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.util.Date;
 
@@ -10,16 +12,14 @@ public class Submission {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String fileName;
+    private String type;
+    private String fileStoragePath; // File storage path
+    @CreationTimestamp
+    private Date creationDate;
+    @UpdateTimestamp
+    private Date lastUpdated;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "submission_date")
-    private Date submissionDate;
-
-    @Column(name = "file_url")
-    private String fileUrl;
-
-    @Column(name = "comments")
-    private String comments;
 
     // Many-to-one relationship with Student
     @ManyToOne
@@ -33,6 +33,63 @@ public class Submission {
     public Submission() {
     }
 
+    public Submission(Long id, String fileName, String type,
+                      String fileStoragePath, Date creationDate,
+                      Date lastUpdated, Student student, Assessment assessment) {
+        this.id = id;
+        this.fileName = fileName;
+        this.type = type;
+        this.fileStoragePath = fileStoragePath;
+        this.creationDate = creationDate;
+        this.lastUpdated = lastUpdated;
+        this.student = student;
+        this.assessment = assessment;
+    }
+
+    // Builder class
+    public static class Builder {
+        private String fileName;
+        private String type;
+        private String fileStoragePath;
+        private Student student;
+        private Assessment assessment;
+
+        public Builder fileName(String fileName) {
+            this.fileName = fileName;
+            return this;
+        }
+
+        public Builder type(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder fileStoragePath(String fileStoragePath) {
+            this.fileStoragePath = fileStoragePath;
+            return this;
+        }
+
+        public Builder student(Student student) {
+            this.student = student;
+            return this;
+        }
+
+        public Builder assessment(Assessment assessment) {
+            this.assessment = assessment;
+            return this;
+        }
+
+        public Submission build() {
+            Submission submission = new Submission();
+            submission.fileName = this.fileName;
+            submission.type = this.type;
+            submission.fileStoragePath = this.fileStoragePath;
+            submission.student = this.student;
+            submission.assessment = this.assessment;
+            return submission;
+        }
+    }
+
     public Long getId() {
         return id;
     }
@@ -41,28 +98,44 @@ public class Submission {
         this.id = id;
     }
 
-    public Date getSubmissionDate() {
-        return submissionDate;
+    public String getFileName() {
+        return fileName;
     }
 
-    public void setSubmissionDate(Date submissionDate) {
-        this.submissionDate = submissionDate;
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
     }
 
-    public String getFileUrl() {
-        return fileUrl;
+    public String getType() {
+        return type;
     }
 
-    public void setFileUrl(String fileUrl) {
-        this.fileUrl = fileUrl;
+    public void setType(String type) {
+        this.type = type;
     }
 
-    public String getComments() {
-        return comments;
+    public String getFileStoragePath() {
+        return fileStoragePath;
     }
 
-    public void setComments(String comments) {
-        this.comments = comments;
+    public void setFileStoragePath(String fileStoragePath) {
+        this.fileStoragePath = fileStoragePath;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
     }
 
     public Student getStudent() {
