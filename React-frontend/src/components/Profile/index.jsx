5cfa9ac09e213e1dcd48fb130f5b73209d4d1f6c
Kanishka Udapitiya <kanishka.udapitiya98@gmail.com>
2024-05-05 20:17:33 +0530

    profile page UI fix

--- React-frontend/src/components/Profile/index.jsx
+++ React-frontend/src/components/Profile/index.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
+import './styles.css';
 
 function ProfileDetails({ user }) {
     return (
-        <div>
-            <h1>Profile Details</h1>
-            <p><strong>ID:</strong> {user.id}</p>
-            <p><strong>First Name:</strong> {user.firstName}</p>
-            <p><strong>Last Name:</strong> {user.lastName}</p>
-            <p><strong>Username:</strong> {user.username}</p>
-            <p><strong>Role:</strong> {user.role}</p>
-            <p><strong>Enabled:</strong> {user.enabled ? 'Yes' : 'No'}</p>
-            <p><strong>Authorities:</strong> {user.authorities.map(auth => auth.authority).join(', ')}</p>
-            <p><strong>Credentials Non-Expired:</strong> {user.credentialsNonExpired ? 'Yes' : 'No'}</p>
-            <p><strong>Account Non-Expired:</strong> {user.accountNonExpired ? 'Yes' : 'No'}</p>
-            <p><strong>Account Non-Locked:</strong> {user.accountNonLocked ? 'Yes' : 'No'}</p>
+        <div className="profile-details">
+            <h1 style={{ textAlign: 'left', fontWeight: 'bold', position: 'absolute', paddingRight: '1%', marginLeft: '42%' }}>Profile Details</h1>
+            <div className='profile-details-info'>
+                <p><strong>ID:</strong></p>
+                <p>{user.id}</p>
+                <p><strong>First Name:</strong></p>
+                <p>{user.firstName}</p>
+                <p><strong>Last Name:</strong></p>
+                <p>{user.lastName}</p>
+                <p><strong>Username:</strong></p>
+                <p>{user.username}</p>
+                <p><strong>Role:</strong></p>
+                <p>{user.role}</p>
+                <p><strong>Enabled:</strong></p>
+                <p>{user.enabled ? 'Yes' : 'No'}</p>
+                <p><strong>Authorities:</strong></p>
+                <p>{user.authorities.map(auth => auth.authority).join(', ')}</p>
+                <p><strong>Credentials Non-Expired:</strong></p>
+                <p>{user.credentialsNonExpired ? 'Yes' : 'No'}</p>
+                <p><strong>Account Non-Expired:</strong></p>
+                <p>{user.accountNonExpired ? 'Yes' : 'No'}</p>
+                <p><strong>Account Non-Locked:</strong></p>
+                <p>{user.accountNonLocked ? 'Yes' : 'No'}</p>
+            </div>
         </div>
     );
 }
