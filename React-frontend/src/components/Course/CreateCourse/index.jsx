ccff6e944604c954a730c3d38ce0646024ab8245
Kanishka Udapitiya <kanishka.udapitiya98@gmail.com>
2024-05-07 22:47:06 +0530

    create course UI fix

--- React-frontend/src/components/Course/CreateCourse/index.jsx
+++ React-frontend/src/components/Course/CreateCourse/index.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
+import "./styles.css";
 
 function CreateCourse({ courseData }) {
   const { courseName, setCourseName, description, setDescription, price, setPrice, handleCreateCourse } = courseData;
 
   return (
-    <form onSubmit={handleCreateCourse}>
-      <label>
-        Course Name:
-        <input type="text" value={courseName} onChange={(e) => setCourseName(e.target.value)} required />
-      </label>
-      <br />
-      <label>
-        Course Description:
-        <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} required />
-      </label>
-      <br />
-      <label>
-        Course Price:
-        <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} required />
-      </label>
-      <br />
-      <button type="submit">Create Course</button>
-    </form>
+    <div className='page_body'><h3 style={{fontWeight:'bold', color:'#0F1035', textAlign:'left'}}>Add new course</h3>
+    <div className="card-content">
+      <form onSubmit={handleCreateCourse}>
+        <div className="page_input">
+            <label>
+              Course Name:
+              <div className="input-group">
+                <input type="text" value={courseName} onChange={(e) => setCourseName(e.target.value)} required />
+              </div>
+            </label>
+            <br />
+            <label>
+              Course Description:
+              <div className="input-group">
+              <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} required />
+              </div>
+            </label>
+            <br />
+            <label>
+              Course Price:
+              <div className="input-group">
+              <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} required />
+              </div>
+            </label>
+            <br />
+            <button type="submit" className="btn btn-primary">Create</button>
+        </div>
+      </form>
+    </div>
+    </div>
   );
 }
 
